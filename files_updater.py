0908413aeda3d6f5d113be35bd7fe0db443faf8e
Serhii <serhii.synohub@gmail.com>
2017-02-27 16:52:34 +0200

    Ask user dialog and check modifying date of files

--- files_updater.py
+++ files_updater.py
@@ -1,33 +1,72 @@
 import glob, os, shutil, log_to_file, datetime
 from os import path
+from tkinter import *
+from tkinter import messagebox
 
+#define variables
 now=datetime.datetime.now()
 success='success_log'
 error='error_log'
 
+#current directory variable to create log files later
 cwd=os.getcwd()
 
+#get settings from file to copy from path1 to path2
 setings_file=open('setings.upd','r')
 setings=setings_file.readlines()
 setings_file.close()
 
-
+#initialize path variables with data from settings file
 for line in setings:
     seting=line.rstrip('\n').split('=')
 
     if seting[0]=='dirpath_to_update':
         dirpath_to_update=seting[1]
     elif seting[0]=='dirpath_from_update':
-        dirpath_from_update=seting[1]      
+        dirpath_from_update=seting[1]
+
+#update file function
+def updateFile(from_dir,to_dir, fileName):
+    #copy file with seving metadata
+    shutil.copy2(dirpath_from_update+'\\'+fileName,dirpath_to_update,follow_symlinks=False)
+    #write to success log
+    log_to_file.write_log(success,now.strftime("%Y-%m-%d %H:%M:%S ")+fileName+'\tis copied from:\t'+\
+                          dirpath_from_update+'\tto:\t'+dirpath_to_update)
+    #print info to console
+    print(now.strftime("%Y-%m-%d %H:%M:%S ")+fileName+'\tis copied from:\t'+\
+          dirpath_from_update+'\tto:\t'+dirpath_to_update)
+
+#call dialog box for user choise if he|she wants to update right now
+def callback(processName):
+    #the askyesno function opens empty tk window, so we need explicitly call and close it
+    Tk().withdraw()
+    if messagebox.askyesno('Предложение', 'Хотите обновить программу %s сейчас?\nПрограмма будет закрыта!'\
+                           % (processName)):
+        os.system('taskkill /im %s' % (processName))
         
+        updateFile(dirpath_from_update,dirpath_to_update,processName)
+    else:
+        messagebox.showinfo('Программа %s' % (processName), \
+                 'Программа не будет обновлена.\nНе забудьте обновить позже!')
+        log_to_file.write_log(error, \
+                              now.strftime("%Y-%m-%d %H:%M:%S ")+\
+                              processName+'\t FAILED!\t FILE IS BUSY!')
+        print(now.strftime("%Y-%m-%d %H:%M:%S ")+\
+              processName+'\t FAILED!\t FILE IS BUSY!')
+
+#main method which runs a script
+#creates list of files to update
 def update():
+
+    #create two lists to compare files in path1 and path2
     
-    os.chdir(dirpath_to_update)
 
     files_to_update=[]
     files_from_update=[]
     updating_list=[]
 
+    os.chdir(dirpath_to_update)
+    
     for file in glob.glob('*.exe'):
         files_to_update.append(file)
 
@@ -36,35 +75,31 @@ def update():
     for file in glob.glob('*.exe'):
         files_from_update.append(file)
 
-
-    #print(files_to_update)
-    #print(files_from_update)
-
+    #compare and fill the files to update list
     for file in files_from_update:
+        
         if (file in files_to_update) and \
-        (path.getctime(dirpath_from_update+'\\'+file)>\
-         path.getctime(dirpath_to_update+'\\'+file)):
+           (path.getmtime(dirpath_from_update+'\\'\
+            +file)>path.getmtime(dirpath_to_update+'\\'+file)):
             updating_list.append(file)
+            
         elif not file in files_to_update:
             updating_list.append(file)
 
+    #ch dir for writing logs into
     os.chdir(cwd)
 
+    
+   
     for file in updating_list:
-        try:
-            shutil.copy2(dirpath_from_update+'\\'+file, \
-                        dirpath_to_update,follow_symlinks=False)
-            log_to_file.write_log(success,now.strftime("%Y-%m-%d %H:%M:%S ")+file+'\tis copied from:\t'+ \
-                       dirpath_from_update+'\tto:\t'+dirpath_to_update)
-            print(now.strftime("%Y-%m-%d %H:%M:%S ")+file+'\tis copied from:\t'+dirpath_from_update+ \
-                  '\tto:\t'+dirpath_to_update)
-            
-        except IOError:
-            log_to_file.write_log(error,now.strftime("%Y-%m-%d %H:%M:%S ")+file+'\t FAILED!\t FILE IS BUSY!')
-            print(now.strftime("%Y-%m-%d %H:%M:%S ")+file+'\t FAILED!\t FILE IS BUSY!')
         
+        try:
+            if len(updating_list)==0:
+                print('All is up to date.')
+            else:
+                updateFile(dirpath_from_update,dirpath_to_update,file)
+        except IOError:
+            callback(file)
 
     if len(updating_list)==0:
-        print('All is up to date.')
-
-
+        print('All files is up to date')
