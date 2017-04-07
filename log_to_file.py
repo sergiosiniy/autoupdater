a36f7086f086bbb43d6a9b4629cb492173e17e74
Serhii Synohub <serhii.synohub@gmail.com>
2017-04-07 15:03:26 +0300

    code documentation

--- log_to_file.py
+++ log_to_file.py
@@ -1,4 +1,10 @@
 def write_log(logname,string):
+    """Creates log dile and appends log entries to it.
+
+        Args:
+            logname: Is used as name of log file to create or append.
+            
+    """
 
     with open(logname + '.log','a') as log:
             log.write(string+'\n')            
