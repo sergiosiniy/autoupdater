6824b17ff3bd758925e2412e20a568f3967d2236
Serhii Synohub <serhii.synohub@gmail.com>
2017-04-24 14:36:25 +0300

    network disk auto connect, checking network path for availability, extended settings for network drive connection and displaying faill connect message

--- execute.py
+++ execute.py
@@ -3,14 +3,17 @@ from self_update import self_update
 
 #create object for updating modules
 selfupdater = self_update()
+#create object for updating files
+updater = programs_updater()
+#check disk availability
+isavailable = updater.check_disk()
 
 #if program is updated or update isn't needed - update files
-if selfupdater.update_modules():
-    updater = programs_updater()    
+if isavailable:
+    mup_success = selfupdater.update_modules()
+else:
+    mup_success = False
+    
+
+if mup_success:
     updater.update()
-
-
-
-
-
-
